c6150d2cdc21ec7ac76f612d86b6f51eeb592dad
Aditya Raj <aditya.rajb@gmail.com>
2021-12-20 11:35:36 +0530

    add models

--- twitclone/models.py
+++ twitclone/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
-# Create your models here.
+class User(AbstractUser):
+    username = models.CharField(max_length=36, unique=True, validators=[
+        RegexValidator('[a-zA-Z0-9\-_]+', 'Username can only contain letters, numbers, dashes and underscores')
+    ])
+    email = models.EmailField()
+    bio = models.TextField(max_length=280)
+    profile_picture = models.ImageField()
+
+
+class Tweet(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    quoted_tweet = models.ForeignKey('self', blank=True, null=True, on_delete=models.SET_NULL)
+    text = models.TextField(max_length=280)
