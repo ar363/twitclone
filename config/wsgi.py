7693e09f8b3ffe2882d6b4c4233de4057522adf3
Aditya Raj <aditya.rajb@gmail.com>
2021-12-22 15:53:23 +0530

    revert adding dotenv to wsgi

--- config/wsgi.py
+++ config/wsgi.py
@@ -9,10 +9,7 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 
 import os
 
-import dotenv
 from django.core.wsgi import get_wsgi_application
-
-dotenv.read_dotenv(os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
