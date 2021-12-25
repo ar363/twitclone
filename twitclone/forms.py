ba45cb380b87b9291c29df36a14a7e88839616cc
Aditya Raj <aditya.rajb@gmail.com>
2021-12-25 15:28:15 +0530

    pfp size limit not wokring

--- twitclone/forms.py
+++ twitclone/forms.py
@@ -28,12 +28,3 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'profile_picture', 'bio']
-    
-    def clean_profile_picture(self):
-        pfp = self.cleaned_data.get('profile_picture', False)
-        if pfp:
-            if pfp._size > 0.25*1024*1024:
-                raise ValidationError("Image file too large ( > 250kb )")
-            return pfp
-        else:
-            return None
